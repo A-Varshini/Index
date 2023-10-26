7701e1855cadb8b90f26ac342cb8e88da1bc53c1
A-Varshini <varshupretty04@gmail.com>
2023-10-26 12:54:26 +0530

    first commit

---
+++ src/Main.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div>
+      
+    <center><h2>TASK TABLE</h2></center>
+
+<table>
+    <thead>
+        <tr>
+            <th>Task Name</th>
+            <th>Repository Link</th>
+            <th>Deployment Link</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Task 1</td>
+            <td><a href="https://github.com/A-Varshini/task1.git">Repository 1</a></td>
+            <td><a href="https://task1-sigma-one.vercel.app/">Deployment 1</a></td>
+        </tr>
+        <tr>
+            <td>Task 2</td>
+            <td><a href="https://github.com/A-Varshini/task2.git">Repository 2</a></td>
+            <td><a href="https://task2-psi-lac.vercel.app/">Deployment 2</a></td>
+        </tr>
+        <tr>
+            <td>Task 3</td>
+            <td><a href="https://github.com/A-Varshini/task3.git">Repository 3</a></td>
+            <td><a href="https://task3-five-omega.vercel.app/">Deployment 3</a></td>
+        </tr>
+        <tr>
+            <td>Task 4</td>
+            <td><a href="https://github.com/A-Varshini/task4.git">Repository 4</a></td>
+            <td><a href="https://task4-six-chi.vercel.app/">Deployment 4</a></td>
+        </tr>
+        <tr>
+            <td>Task 5</td>
+            <td><a href="https://github.com/A-Varshini/task5.git">Repository 5</a></td>
+            <td><a href="https://task5-qmz14edsj-a-varshinis-projects.vercel.app/">Deployment 5</a></td>
+        </tr>
+        <tr>
+            <td>Portfolio</td>
+            <td><a href="https://github.com/A-Varshini/Portfolio-task6.git">Repository 6</a></td>
+            <td><a href="https://portfolio-task6-3-git-main-a-varshinis-projects.vercel.app/">Deployment 6</a></td>
+        </tr>
+    </tbody>
+</table>
+<center><h2>DONE BY:</h2></center>
+<center><h2>  A SRI VARSHINI</h2></center>
+<center><h2> 21BCE8109</h2></center>
+    </div>
+  )
+}
+
+export default Main
